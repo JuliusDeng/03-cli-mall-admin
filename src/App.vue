30cf1a0884a1ed1e194066c5050e31812e8a1854
JuliusDeng <1576554007@qq.com>
2020-10-31 16:50:54 +0800

    bootstrap4引入

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	  <div class="card">
+		  <div class="card-header">但是</div>
+		  <div class="card-body">但是</div>
+		  <div class="card-footer">但是</div>
+	  </div>
+	  
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+	  
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
